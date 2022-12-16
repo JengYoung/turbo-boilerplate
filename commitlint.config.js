8074c56a31f039e7f3d3090159984de3dfed11ee
JengYoung <young_pallete@naver.com>
2022-12-16 17:38:32 +0900

    refactor(configuration): commitlint, prepare-commit-msg CI hook

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,27 +1,14 @@
+const cz = require('./.cz-config.js');
+
+const TYPE_ALWAYS = 'always';
+
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [2, 'always', 72],
-    'body-leading-blank': [2, 'always'],
-    'body-max-length': [2, 'always', 400],
-    'footer-leading-blank': [2, 'always'],
-    'type-enum': [
-      2,
-      'always',
-      [
-        'feat',
-        'fix',
-        'style',
-        'chore',
-        'ci',
-        'refactor',
-        'revert',
-        'test',
-        'remove',
-        'move',
-        'docs',
-        'perf',
-      ],
-    ],
+    'header-max-length': [2, TYPE_ALWAYS, 72],
+    'body-leading-blank': [2, TYPE_ALWAYS],
+    'body-max-length': [2, TYPE_ALWAYS, 400],
+    'footer-leading-blank': [2, TYPE_ALWAYS],
+    'type-enum': [2, TYPE_ALWAYS, cz.types.map(({ value }) => value)],
   },
 };
